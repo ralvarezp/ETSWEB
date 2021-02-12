730f2e797dc52b1f65a73f4f184fc0544fe05e72
ralvarezp <78942971+ralvarezp@users.noreply.github.com>
2021-02-12 14:16:38 -0600

    Update CtrlSesion.js

--- Github/ETS/js/CtrlSesion.js
+++ Github/ETS/js/CtrlSesion.js
@@ -1,33 +1,21 @@
 import { DaoUsuarios } from "./DaoUsuarios.js";
 
-/** @typedef {Object} UsuarioAutorizado
- * @property {string} email
- * @property {string} nombre
- * @property {string} urlFoto
- * @property {Set<string>} privilegios  */
 
 export class CtrlSesión {
-  /** @param {Object} auth Sistema de autenticación de Firebase.
-   * @param {Object} provider Proveedor de autenticación de Firebase.
-   * @param {DaoUsuarios} daoUsuarios */
+ 
   constructor(auth, provider, daoUsuarios) {
     this._auth = auth;
     this._provider = provider;
     this._daoUsuarios = daoUsuarios;
   }
 
-  /**
-   * @param {string} privilegio
-   * @returns {Promise<UsuarioAutorizado>}  */
+
   async protege(privilegio) {
     return new Promise((resolve, reject) => {
-      /* Escucha cambios de usuario. Recibe una función que se invoca cada que
-       * hay un cambio en la autenticación y recibe el modelo con las
-       * características del usuario o null si no ha iniciado sesión .*/
+     
       this._auth.onAuthStateChanged(async usuarioAuth => {
         if (usuarioAuth && usuarioAuth.email) {
-          // Usuario aceptado.
-          /** @type {Set<string>} */
+         
           let privilegios = new Set();
           const usuario = await this._daoUsuarios.busca(usuarioAuth.email);
           if (usuario) {
@@ -54,13 +42,12 @@ export class CtrlSesión {
             reject(new Error("El usuario no está registrado."));
           }
         } else {
-          // No ha iniciado sesión. Pide datos para iniciar sesión.
+         
           this._auth.signInWithRedirect(this._provider);
-          //auth.signInWithPopup(provider);
-          //auth.signInAnonymously();
+         
         }
       },
-        // Función que se invoca si hay un error al verificar el usuario.
+       
         reject);
     });
   }
