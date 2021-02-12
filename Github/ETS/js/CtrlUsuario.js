618e464b68c076f877feea23e1170cb81750384c
ralvarezp <78942971+ralvarezp@users.noreply.github.com>
2021-02-12 14:17:17 -0600

    Update CtrlUsuario.js

--- Github/ETS/js/CtrlUsuario.js
+++ Github/ETS/js/CtrlUsuario.js
@@ -6,21 +6,13 @@ import { InfoPasatiempo } from "./InfoPasatiempo.js";
 import { InfoPrivilegio } from "./InfoPrivilegio.js";
 import { InfoUsuario } from "./InfoUsuario.js";
 
-/** @extends {CtrlAbc<InfoUsuario>} */
 export class CtrlUsuarios extends CtrlAbc {
-  /** @param {string} mensajeNoEncontrado
-   * @param {DaoUsuarios} daoUsuarios
-   * @param {DaoPasatiempos} daoPasatiempos
-   * @param {DaoPrivilegios} daoPrivilegios */
   constructor(mensajeNoEncontrado, daoUsuarios, daoPasatiempos,
     daoPrivilegios) {
     super(mensajeNoEncontrado, daoUsuarios);
     this._daoPasatiempos = daoPasatiempos;
     this._daoPrivilegios = daoPrivilegios;
   }
-  /** @param {(error: Error)=>void} callbackError
-   * @param {(pasatiempos:InfoPasatiempo[])=>void} callbackPasatiempos
-   * @param {(privilegios:InfoPrivilegio[])=>void} callbackPrivilegios */
   foráneas(callbackError, callbackPasatiempos, callbackPrivilegios) {
     this._daoPasatiempos.consulta(callbackError, callbackPasatiempos);
     this._daoPrivilegios.consulta(callbackError, callbackPrivilegios);
